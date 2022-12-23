3f577276787e969bb1c46203ce5bc41212d56f74
hoimohu <basotahoi@gmail.com>
2022-12-23 15:28:14 +0900

    option追加

--- script.js
+++ script.js
@@ -8,10 +8,29 @@
         });
     const output = document.getElementById('output');
     const viewcode = document.getElementById('viewcode');
+    const output_option = document.getElementById('output_option');
+    let option = 'none';
+    output_option.addEventListener('change', () => {
+        option = output_option.value;
+        convert();
+    });
     function convert() {
         const code = mdread(input.value);
-        output.innerHTML = format(code);
-        viewcode.innerText = format(code);
+        switch (option) {
+            case 'br':
+                output.innerHTML = format(code);
+                viewcode.innerText = format(code);
+                break;
+            case 'fewbr':
+                output.innerHTML = format(code, true);
+                viewcode.innerText = format(code, true);
+                break;
+            default:
+            case 'none':
+                output.innerHTML = code;
+                viewcode.innerText = code;
+                break;
+        }
     }
     input.addEventListener('input', convert);
 })();
